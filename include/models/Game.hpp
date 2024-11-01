70182beae3f3dee056cc319c7b4366ebec974bfe
kodesun <168811279+kodesun@users.noreply.github.com>
2024-11-01 08:55:06 -0700

    add: Game class

---
+++ include/models/Game.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <SFML/Graphics/Font.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+
+#include <SFML/System/Clock.hpp>
+
+#include <string>
+
+namespace satisfying_ball {
+    class Game {
+        public:
+            Game(int width, int height, std::string title);
+            void Run();
+
+        private:
+            const float dt = 1.0f/60.0f;
+            std::string title;
+            sf::RenderWindow window;
+            sf::Clock clock;
+
+            sf::Font font;
+
+            bool paused = false;
+
+            void HandleInput();
+            void Update();
+            void Draw(float dt);
+    };
+}

b1baf286a7be9a07a88620a6da1a3110bb00ac4a
kodesun <168811279+kodesun@users.noreply.github.com>
2024-11-05 12:26:10 -0700

    add: basic definitions

---
+++ definitions.py
@@ -0,0 +1,22 @@
+# Well .. definitions of important constants
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+# Following a 9:16 (vertical video) aspect ratio
+WINDOW_WIDTH, WINDOW_HEIGHT = 480, 854 
+WINDOW_TITLE = "Satisfying Ball"
+
+FRAMES_PER_SECOND = 120
+
+BACKGROUND_COLOR = BLACK 
+
+## Boundary
+BOUNDARY_LINE_THICKNESS = 3
+BOUNDARY_MARGIN = 10
+BOUNDARY_RADIUS = (WINDOW_WIDTH//2) - BOUNDARY_MARGIN
+BOUNDARY_COLOR = WHITE 

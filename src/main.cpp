e3be674d87b6c8ea82a0e32f552daf2232d4cc3d
kodesun <168811279+kodesun@users.noreply.github.com>
2024-11-04 23:23:29 -0700

    delete: restarting -- easier implementation idea

--- src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "models/Game.hpp"
-#include "DEFINITIONS.hpp"
-
-int main (int argc, char *argv[]) {
-    satisfying_ball::Game app(WINDOW_WIDTH, WINDOW_HEIGHT, "Satisfying Ball");
-    app.Run();
-    return 0;
-}

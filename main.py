b31bdd10baf4f7753853d4c377da2ffe50467355
kodesun <168811279+kodesun@users.noreply.github.com>
2024-11-06 22:46:43 -0700

    add: ball & boundary updating and drawing

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = 'hide'
 
-from models import Display, Boundary
+from models import Display, Boundary, Ball
 
 if __name__ == "__main__":
     display = Display() 
+
+    ball = Ball() 
     boundary = Boundary()
 
     while True:
         display.handle_input()
         display.clear()
+        ball.update()
+        display.draw(ball)
         display.draw(boundary)
         display.show()
